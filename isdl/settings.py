a753eedb7b9310f0dd9197630c8c4a140065d1b3
maanas2403 <108995876+maanas2403@users.noreply.github.com>
2023-11-24 19:57:23 +0530

    Update settings.py

--- isdl/settings.py
+++ isdl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5^&k)u9a56gwyp!e6om=)uk=5$4@f-wkj*#j(w*yxggl*xzlrg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
