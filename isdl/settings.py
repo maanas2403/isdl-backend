50364cc1feef2ea7f9950a8ba64813991d0d1e1f
maanas2403 <108995876+maanas2403@users.noreply.github.com>
2023-11-24 21:24:45 +0530

    Update settings.py

--- isdl/settings.py
+++ isdl/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+CORS_ALLOWED_ORIGINS = [
+    "https://limanage.vercel.app",
+    # Add other allowed origins as needed
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL=True
